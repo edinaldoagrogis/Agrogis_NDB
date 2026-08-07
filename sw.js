049b138a14dac2e2ad7f65268602d3ede664e4b4
Edinaldo <edinaldo@geoportal.com>
2026-08-07 02:56:36 -0300

    Remove completamente o plugin de rotacao e bussola

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agrogis-v19';
+const CACHE_NAME = 'agrogis-v20';
 
 // Core assets to pre-cache when the Service Worker installs
 const PRECACHE_URLS = [
@@ -18,8 +18,7 @@ const PRECACHE_URLS = [
     'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.min.js',
     'https://unpkg.com/@turf/turf@6/turf.min.js',
     'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
-    'https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.js',
-    'https://unpkg.com/leaflet-rotate@0.2.8/dist/leaflet-rotate.js'
+    'https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.js'
 ];
 
 self.addEventListener('install', event => {
